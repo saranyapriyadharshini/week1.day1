ee49afb77128dd8d754c8f7c981ee079e9ae4f0d
saranyapriyadharshini <saranya.c90@gmail.com>
2022-04-16 21:50:34 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -4,18 +4,20 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		int a = 13;
 		boolean prime = false;
-		for (int i = 2; i <= a / 2; ++i) {
+		for (int i = 2; i < a; ++i) {
 			if (a % i == 0) {
 				prime = true;
+				System.out.println("Its not a prime number");
 				break;
 
 			}
 
 			if (!prime) {
-				System.out.println(a+ "Its a prime number");
-			} else {
-				System.out.println("Its not a prime number");
+				System.out.println("Its a prime number");
+
 			}
 		}
 	}
 }
+
+
